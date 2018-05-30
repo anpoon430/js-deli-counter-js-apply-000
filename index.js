dad2ce0238c13940689074d58bf1635713696e91
Andy Poon <anpoon430@gmail.com>
2018-05-30 19:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ console.log(takeANumber(deliLine))
 console.log(currentLine(deliLine))
 console.log(nowServing(deliLine))
 console.log(currentLine(deliLine))
+console.log(takeANumber(deliLine))
 
 function nowServing(line){
   if (line.length === 0){
